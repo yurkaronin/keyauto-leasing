71afb9197556d50091013be45b7c0551a4bfbddc
RoninsWork <yurkaronin@gmail.com>
2022-05-27 14:24:52 +0300

    Закончил модальные окна

--- js/main.js
+++ js/main.js
@@ -12,3 +12,24 @@ const customSelects = () => {
 };
 
 customSelects();
+
+MicroModal.init({
+  openTrigger: 'data-custom-open',
+  closeTrigger: 'data-custom-close',
+  disableScroll: true,
+  disableFocus: true,
+  awaitOpenAnimation: true,
+  awaitCloseAnimation: true
+})
+$('[data-custom-open]').click(function() {
+  $('.modal [name=form]').val($(this).data('form'))
+})
+$('[data-custom-close]').click(function() {
+  $('.modal [name=form]').val('')
+})
+
+$('.home-slider__heading').each(function() {
+  let text  = $(this).text().split(' '),
+      first = text.shift()
+  $(this).html(`${first} <br><span>${text.join(' ')}</span>`)
+})
