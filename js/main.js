28b2b30aa7970cebd0b3200fdbf78660b3952a6f
RoninsWork <yurkaronin@gmail.com>
2022-05-27 16:29:02 +0300

    Правки по JS
    Правки скрипта проверки ширины экрана
    Обернул всё в условие загрузки страницы (dom)

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
-const customSelects = () => {
-  const elements = document.querySelectorAll('.js-choice');
-  elements.forEach(el => {
-    const choices = new Choices(el, {
-      searchEnabled: false,
-      removeItems: true,
-      removeItemButton: true,
-      itemSelectText: '',
+document.addEventListener("DOMContentLoaded", () => {
+  let filterShow = document.querySelector('.filter-show');
+  let filterList = document.querySelector('.filter-list');
+  if (filterShow) {
+    filterShow.onclick = function () {
+      filterList.classList.toggle('show-all');
+    };
+
+  }
+
+  if (window.innerWidth <= 670) {
+    filterList.classList.add('show-all');
+  } else {
+    filterList.classList.remove('show-all');
+  }
+
+  const customSelects = () => {
+    const elements = document.querySelectorAll('.js-choice');
+    elements.forEach(el => {
+      const choices = new Choices(el, {
+        searchEnabled: false,
+        removeItems: true,
+        removeItemButton: true,
+        itemSelectText: '',
+      });
     });
-  });
 
-};
-
-customSelects();
-
-MicroModal.init({
-  openTrigger: 'data-custom-open',
-  closeTrigger: 'data-custom-close',
-  disableScroll: true,
-  disableFocus: true,
-  awaitOpenAnimation: true,
-  awaitCloseAnimation: true
-})
-
-/* const mainTable = document.querySelector('.main__table');
-if (mainTable.scrollLeft) {
-  console.log('wow');
-} */
-
-let filterShow = document.querySelector('.filter-show');
-let filterList = document.querySelector('.filter-list');
-if(filterShow) {
-  filterShow.onclick = function() {
-    filterList.classList.toggle('show-all');
-    // filterList.style.height = 90;
-    // filterList.style.overflow = hidden;
   };
 
-}
+  customSelects();
 
+  MicroModal.init({
+    openTrigger: 'data-custom-open',
+    closeTrigger: 'data-custom-close',
+    disableScroll: true,
+    disableFocus: true,
+    awaitOpenAnimation: true,
+    awaitCloseAnimation: true
+  })
+});
